1c773c27de79566099854d4ab6b6c79596c2530d
Your Name <you@example.com>
2020-12-25 12:02:15 +0100

    remove

--- cronjob.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#PODCASTDIR=/mnt/usb1/podcast.test
-
-PODCASTDIR=$1
-
-cd /home/podcast/rssfeeder
-./rssfeeder.sh $PODCASTDIR bp.conf
--- testdown.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#PODCASTDIR=/mnt/usb1/podcast.test
-#PODCASTDIR=/mnt/usb/podcast
-PODCASTDIR=/tmp/podcast
-cd /home/podcast/rssfeeder
-./rssfeeder.sh $PODCASTDIR template/bptest.conf
